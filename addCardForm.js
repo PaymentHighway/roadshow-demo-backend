3ced4aff415f740d2376d93173951a7aac98178f
Pauli Kostamo <pauli.kostamo@paymenthighway.fi>
2019-11-22 13:29:55 +0200

    remove some "useless" code

--- addCardForm.js
+++ addCardForm.js
@@ -5,33 +5,25 @@ const FormBuilder = paymentHighway.FormBuilder;
 
 module.exports.add = async (event) => {
 
-  const account = process.env.ACCOUNT;
-  const merchantId = process.env.MERCHANT_ID;
-  const testKey = process.env.KEY;
-  const testSecret = process.env.SECRET;
-  const baseUrl = process.env.SERVICE_URL;
-  const language = "EN";
-
-  logJson(event.queryStringParameters);
-
   const user = event.queryStringParameters.user;
-  const successUrl = event.queryStringParameters.success;
-  const failureUrl = event.queryStringParameters.failure;
-  const cancelUrl = event.queryStringParameters.cancel;
-
   const webhookSuccessUrl = (process.env.URL + 'card/add?user=' + user).trim();
   const webhookFailureUrl = (process.env.URL + 'card/add?user=' + user).trim();
   const webhookCancelUrl = (process.env.URL + 'card/add?user=' + user).trim();
 
-  log(user, successUrl, failureUrl, cancelUrl, webhookSuccessUrl, webhookFailureUrl, webhookCancelUrl);
-
-  const formBuilder = new FormBuilder('GET', testKey, testSecret, account, merchantId, baseUrl);
+  const formBuilder = new FormBuilder(
+    'GET',
+    process.env.KEY,
+    process.env.SECRET,
+    process.env.ACCOUNT,
+    process.env.MERCHANT_ID,
+    process.env.SERVICE_URL
+  );
 
   const formContainer = formBuilder.generateAddCardParameters(
-    successUrl,
-    failureUrl,
-    cancelUrl,
-    language,
+    event.queryStringParameters.successUrl,
+    event.queryStringParameters.failureUrl,
+    event.queryStringParameters.cancelUrl,
+    event.queryStringParameters.language,
     false,
     undefined,
     undefined,
@@ -44,9 +36,6 @@ module.exports.add = async (event) => {
 
   const parameters = formContainer.nameValuePairs;
 
-  logJson(parameters);
-  log(parameters.find(parameter => parameter.first === 'sph-request-id' ));
-
   const url = formContainer.getAction() + "?" + parametersToQueryString(parameters);
 
   return { statusCode: 200, body: JSON.stringify({url: url}) };
@@ -56,16 +45,4 @@ function parametersToQueryString(parameters) {
   return parameters.map(x => {
     return x.first + "=" + x.second;
   }).join("&");
-}
-
-function log(...args) {
-  args.forEach((arg) => {
-    console.log(arg);
-  });
-}
-
-function logJson(...args) {
-  args.forEach((arg) => {
-    console.log(JSON.stringify(arg));
-  });
 }
